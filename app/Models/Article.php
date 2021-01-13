b008c31c164fb1b151bcc4e8d5e728f293bb4f5a
Beka Chkhaidze <me@wamu.li>
2021-01-13 21:32:35 +0400

    add Article model

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+    	'name',
+    	'link',
+    	'read',
+    	'read_at'
+    ];
+
 }
+
+
+
