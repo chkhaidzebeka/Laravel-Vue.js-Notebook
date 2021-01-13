c057636ce4d612e8e1a8ec086bf866f50ac86808
Beka Chkhaidze <me@wamu.li>
2021-01-13 23:07:54 +0400

    add vue.js

---
+++ resources/js/vue/app.vue
@@ -0,0 +1,16 @@
+<template>
+	<div>
+		<h1>Vue.js works</h1>
+	</div>
+</template>
+
+<style>
+	h1 {
+		color: #34495e;
+		font-family: Nunito
+	}
+</style>
+
+<script>
+export default {};
+</script>
